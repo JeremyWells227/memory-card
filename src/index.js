c526bb5914497dae0277b3c198a4bba9c58e93bf
Jeremy Wells <jswe227@gmail.com>
2022-05-25 15:12:35 -0400

    Changed host to match repo, removed helmet

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import GameBoard from './components/GameBoard';
 
-import { Helmet } from 'react-helmet';
 import "./index.scss";
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-	<Helmet>
-	<title> Meme Memory Game </title> 
-	<meta charset="utf-8"/> 
-	<link rel="preconnect" href="https://fonts.googleapis.com"/>
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-	<link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet"/>
-	</Helmet>
 	<div>	
 	<Header />
 	</div>
